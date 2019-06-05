19f44ecaf9d3a0bd36d03ce1117e91e875bf2244
wayne391 <s101062219@gmail.com>
2019-06-05 11:39:31 +0800

    use new miditoolkit

--- test.py
+++ test.py
@@ -1,8 +1,10 @@
+import os
 import numpy as np
 from pypianoroll import Multitrack
 from sklearn.externals import joblib 
 from utils import features
 from utils.misc import traverse_dir
+from miditoolkit.midi import parser
 
 PATH_MODEL = 'result/model.pkl'
 
@@ -27,7 +29,7 @@ def identify_multiple_track(pianorolls):
         X = features.extract_features(pianorolls[idx])
         test_x.append(X)
     test_x = np.array(test_x)
-    return testing(test_x )
+    return testing(test_ )
    
 def identify_song(input_obj):
     # loading
@@ -40,16 +42,32 @@ def identify_song(input_obj):
     pianorolls = []
     for track in multi.tracks:
         pianorolls.append(track.pianoroll)
-    ys =  identify_multiple_track(pianorolls)
+    ys = identify_multiple_track(pianorolls)
     return ys
 
 if __name__ == '__main__':
     # testing sample code
-    testing_dir = 'Raw_Data/jazz_realbook'
-    testing_files = traverse_dir(testing_dir)
+    # testing_dir = 'Raw_Data/jazz_realbook'
+    # testing_files = traverse_dir(testing_dir)
 
-    fidx = 0
-    filename = testing_files[fidx]
-    predict_y = identify_song(filename)
+    # fidx = 0
+    # filename = testing_files[fidx]
+    # predict_y = identify_song(filename)
+    # print(predict_y)
 
-    print(predict_y)
+    path_midi = 'test_midis/aladdin-medley-of-all-songs-mid.mid'
+    midi_file = parser.MidiFile(path_midi)
+    num_instr = len(midi_file.instruments)
+
+    pianorolls = []
+    for idx in range(num_instr):
+        pr = midi_file.get_instrument_pianoroll(idx, resample_resolution=24)
+        pianorolls.append(pr)
+
+    # ys = identify_multiple_track(pianorolls)
+    ys = [2, 3, 0, 3, 0, 3, 0, 3, 0, 2, 2, 3, 3, 3, 2, 0, 0, 3, 3, 0]
+
+    print(ys)
+    midx = list(np.where(np.array(ys)==0)[0])
+    print(midx)
+    midi_file.dump(filename='melody.mid', instrument_idx=midx)
